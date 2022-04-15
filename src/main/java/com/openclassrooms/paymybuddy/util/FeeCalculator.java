9ecc6bf4c9247daafd7233da223f7ab41fd23796
Ben Corpro <corbelbenjaminpro@free.fr>
2022-04-15 17:59:14 +0200

    Fees Correction

--- src/main/java/com/openclassrooms/paymybuddy/util/FeeCalculator.java
+++ src/main/java/com/openclassrooms/paymybuddy/util/FeeCalculator.java
@@ -1,13 +1,14 @@
 package com.openclassrooms.paymybuddy.util;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import com.openclassrooms.paymybuddy.constants.Constants;
 
 public class FeeCalculator {
 
   public static BigDecimal feeCalculator(BigDecimal amount) {
-    BigDecimal fee = amount.multiply(Constants.feePercent);
+    BigDecimal fee = amount.multiply(Constants.feePercent).setScale(2, RoundingMode.HALF_DOWN);
     return fee;
   }
   
